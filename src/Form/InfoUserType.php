3f54dc80e31d59b721d97f5207e5f404db5cc2fb
jaouad erradi <jaouaderradi@proton.me>
2024-09-02 15:36:10 +0200

    Add validation constraints to InfoUserType form fields

--- src/Form/InfoUserType.php
+++ src/Form/InfoUserType.php
@@ -9,18 +9,53 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class InfoUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phoneNumber')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('firstName', null, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'First name is required.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 100,
+                        'maxMessage' => 'First name cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
             ])
+            ->add('lastName', null, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Last name is required.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 100,
+                        'maxMessage' => 'Last name cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+            ])
+            ->add('phoneNumber', null, [
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^\d+$/',
+                        'message' => 'Phone number must contain only digits.',
+                    ]),
+                    new Assert\Length([
+                        'min' => 10,
+                        'max' => 10,
+                        'minMessage' => 'Phone number must be at least {{ limit }} characters long.',
+                        'maxMessage' => 'Phone number cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+            ])
+            // ->add('user', EntityType::class, [
+            //     'class' => User::class,
+            //     'choice_label' => 'id',
+            // ])
         ;
     }
 
