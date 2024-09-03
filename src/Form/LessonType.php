6401619cab2c39cf74629eec0f8e41eaa833f92f
jaouad erradi <jaouaderradi@proton.me>
2024-09-03 14:51:21 +0200

    Added fields for title, content, timestamps, and course relationship in LessonType form class

---
+++ src/Form/LessonType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use App\Entity\Lesson;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
+class LessonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('course', EntityType::class, [
+                'class' => Course::class,
+                'choice_label' => 'title',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lesson::class,
+        ]);
+    }
+}
