b438aca970ce977a6d30b055187a633538f5e48d
jaouad erradi <jaouaderradi@proton.me>
2024-09-03 14:37:26 +0200

    Add image upload & constraints functionality

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -11,9 +11,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 #[Route('/course')]
 final class CourseController extends AbstractController
 {
+
+    private $tokenStorage;
+    private $authorizationChecker;
+
+    public function __construct(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
     #[Route(name: 'app_course_index', methods: ['GET'])]
     public function index(CourseRepository $courseRepository): Response
     {
@@ -23,24 +37,53 @@ final class CourseController extends AbstractController
     }
 
     #[Route('/new', name: 'app_course_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $course = new Course();
-        $form = $this->createForm(CourseType::class, $course);
-        $form->handleRequest($request);
+public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger, TokenStorageInterface $tokenStorage): Response
+{
+    $course = new Course();
+    $form = $this->createForm(CourseType::class, $course);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($course);
-            $entityManager->flush();
+    if ($form->isSubmitted() && $form->isValid()) {
 
-            return $this->redirectToRoute('app_course_index', [], Response::HTTP_SEE_OTHER);
+        // Set the authenticated user
+        $course->setUser($tokenStorage->getToken()->getUser());
+
+        // Handle the file upload
+        /** @var UploadedFile $imageFile */
+        $imageFile = $form->get('image')->getData();
+
+        if ($imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+            try {
+                $imageFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                $this->addFlash('danger', 'Failed to upload image: ' . $e->getMessage());
+                return $this->redirectToRoute('app_course_new');
+            }
+
+            // Store the new filename in the database
+            $course->setImage($newFilename);
         }
 
-        return $this->render('course/new.html.twig', [
-            'course' => $course,
-            'form' => $form,
-        ]);
+        // Persist the course entity
+        $entityManager->persist($course);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_course_index');
     }
+
+    return $this->render('course/new.html.twig', [
+        'course' => $course,
+        'form' => $form->createView(),
+    ]);
+}
+
 
     #[Route('/{id}', name: 'app_course_show', methods: ['GET'])]
     public function show(Course $course): Response
