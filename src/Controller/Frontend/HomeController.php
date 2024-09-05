67170f9c9c41502d4d71e315a09948a113ccd77f
jaouad erradi <jaouaderradi@proton.me>
2024-09-05 17:40:43 +0200

    Add dyanamic content (CourseCategory & Course) in home page

--- src/Controller/Frontend/HomeController.php
+++ src/Controller/Frontend/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller\Frontend;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\CourseCategory;
+use App\Entity\Course;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $courses = $entityManager->getRepository(Course::class)->findAll();
+        $categories = $entityManager->getRepository(CourseCategory::class)->findAll();
+
         return $this->render('Frontend/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'courses' => $courses,
+            'categories' => $categories,
         ]);
     }
 }
